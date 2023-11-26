6cace974d7a7d50b8a2f062e43ff5c0b9a08a0fb
digimori <hanaorikuu@gmail.com>
2023-11-26 14:43:49 +0000

    Game Finished. Levels changed to string literals for cleaner code.

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
 
-let gamePattern = [];
 let buttonColours = ["red", "blue", "yellow", "green"];
+
+let gamePattern = [];
 let userClickedPattern = [];
+
+let started = false;
+let level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text(`Level: ${level}`);
+    nextSequence();
+    started = true;
+  }
+});
 
 //1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
 $(".btn").click(function() {
   // Getting the value of the button clicked
     let userChosenColour = $(this).attr("id");
     // Pushing it onto the userClickedPattern array.
+    userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
-    userClickedPattern.push(userChosenColour);
-  });
-   
 
+    checkAnswer(userClickedPattern.length -1); 
+  });
+
+  function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if(userClickedPattern.length === gamePattern.length) {
+        setTimeout(function() {
+          nextSequence();
+        }, 1000)
+      }
+    }else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $('#level-title').text("Game Over, Press any key to restart.");
+      setTimeout(function() {
+        $("body").removeClass("game-over");
+      }, 200);
+      startOver();
+    }
+  }
 
 function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $('#level-title').text(`Level: ${level}`);
   let randomNum = Math.trunc(Math.random() * 4); // Between 0 and 3
   let randomChosenColour = buttonColours[randomNum];
   gamePattern.push(randomChosenColour);
@@ -24,13 +57,6 @@ function nextSequence() {
 $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 playSound(randomChosenColour);
 }
-nextSequence();
-
-function playSound(name) {
-  let audio = new Audio('sounds/' + name + '.mp3');
-  audio.play();
- 
-  }
 
 function animatePress(currentColour) {
   // Get the element with the id matching the colour, add the class
@@ -40,3 +66,14 @@ function animatePress(currentColour) {
     $("#" + currentColour).removeClass("pressed");
   }, 100);
 }
+
+function playSound(name) {
+  let audio = new Audio('sounds/' + name + '.mp3');
+  audio.play();
+  }
+
+  function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+  }
